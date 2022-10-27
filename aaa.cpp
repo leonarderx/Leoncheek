a814242691e16701a68030fd94301917f2e5b0ce
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-10-27 18:21:16 +0300

    Add files via upload

---
+++ aaa.cpp
@@ -0,0 +1,40 @@
+//for2 
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+	//for (int i = 20; i <= 30; i += 2)
+		//cout << i << endl
+
+
+
+//while2
+
+	//int a, b;
+	//cin >> a >> b;
+	//int sum = 0;                      
+	//while (a <= b) {
+	//	if (a % 2 == 0) {
+	//		sum += a;
+	//	}          
+//                                                                    
+	//	a++;
+	//}
+	//cout << sum;
+//}
+
+//for 3 
+
+	//int n; 
+//	int st = 1;
+	//cin >> n;
+//for (int i = 1; i <= n; i++) {
+	//	if (i % 5 != 0) {
+		//	st = st * i;
+	//	}
+	//}
+	//cout << st << ' ';
+
+
+
